592754268799986da2028a9e64fb373130d41dac
João Magdaleno <joaomagdaleno.14@hotmail.com>
2025-12-26 18:37:47 -0300

    feat: configure Android build with Java 17, standardized build paths, and default namespace.

--- universal_notes_flutter/android/build.gradle.kts
+++ universal_notes_flutter/android/build.gradle.kts
@@ -22,16 +22,16 @@ subprojects {
             val android = project.extensions.findByName("android")
             try {
                 // Enforce Java 17 for compileOptions
-                val compileOptions = android.javaClass.getMethod("getCompileOptions").invoke(android)
-                compileOptions.javaClass.getMethod("setSourceCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
+                val compileOptions = android!!.javaClass.getMethod("getCompileOptions").invoke(android)
+                compileOptions!!.javaClass.getMethod("setSourceCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
                 compileOptions.javaClass.getMethod("setTargetCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
 
                 // Enforce Java 17 for kotlinOptions
                 val kotlinOptions = android.javaClass.getMethod("getKotlinOptions").invoke(android)
-                kotlinOptions.javaClass.getMethod("setJvmTarget", String::class.java).invoke(kotlinOptions, "17")
+                kotlinOptions!!.javaClass.getMethod("setJvmTarget", String::class.java).invoke(kotlinOptions, "17")
 
-                val getNamespace = android?.javaClass?.getMethod("getNamespace")
-                val setNamespace = android?.javaClass?.getMethod("setNamespace", String::class.java)
+                val getNamespace = android.javaClass.getMethod("getNamespace")
+                val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
                 if (getNamespace != null && setNamespace != null) {
                     val currentNamespace = getNamespace.invoke(android)
                     if (currentNamespace == null) {
