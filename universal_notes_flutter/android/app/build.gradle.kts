802289365dd596c15a6b4c12ba53fb1e7ba517dc
João Magdaleno <joaomagdaleno.14@hotmail.com>
2025-12-26 22:05:10 -0300

    feat: Add Android Kotlin Gradle build with FlutterFire and Crashlytics, and Windows CMake build configuration.

--- universal_notes_flutter/android/app/build.gradle.kts
+++ universal_notes_flutter/android/app/build.gradle.kts
@@ -18,12 +18,12 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     // Load signing data if present
